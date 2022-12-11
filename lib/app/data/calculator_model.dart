96baa4932fd429fcd39d224f27a397e3b4b83488
Prateek Jain <prateek.jain@squareboat.com>
2022-12-12 00:34:26 +0530

    feat : added calculator ui

---
+++ lib/app/data/calculator_model.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class CalculatorModel{
+
+  CalculatorModel(this.title, this.backGroundColor, this.textColor);
+  final  String title ;
+  final Color backGroundColor;
+  final Color textColor;
+
+
+}
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,21 +1,86 @@
+import 'package:calci/app/data/colors.dart';
+import 'package:calci/app/modules/home/controllers/home_controller.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
-import '../controllers/home_controller.dart';
-
 class HomeView extends GetView<HomeController> {
+  const HomeView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      backgroundColor: primaryBackgroundColor,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20,horizontal: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.history,
+                        color: primaryTextColor,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.straighten,
+                          color: primaryTextColor,
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.bookmark_add_outlined,
+                        color: primaryTextColor,
+                      ),
+                    ),
+                  ],
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.backspace_outlined,
+                    color: primaryTextColor,
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: Container(
+                color: secondaryBackgroundColor,
+                height: 1,
+                width: Get.size.width,
+              ),
+            ),
+            GridView.count(
+              crossAxisCount: 4,
+              padding: EdgeInsets.zero,
+              childAspectRatio: 1 / .8,
+              mainAxisSpacing: 10,
+              shrinkWrap: true,
+              children: List.generate(20, (index) {
+                return CircleAvatar(
+                  backgroundColor: secondaryBackgroundColor,
+                  child: Text(
+                    '',
+                    style: TextStyle(color: primaryTextColor, fontSize: 22),
+                  ),
+                );
+              }),
+            ),
+            const SizedBox(height: 20,),
+          ],
         ),
       ),
     );
