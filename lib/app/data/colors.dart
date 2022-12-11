b3c0eb5f7460b0e0508516bb0d22e4608761a728
Prateek Jain <prateek.jain@squareboat.com>
2022-12-11 23:53:15 +0530

    style : add colors

---
+++ lib/app/data/colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+Color primaryBackgroundColor = const Color(0xff010101);
+Color primaryButtonColor = const Color(0xff437e03);
+Color secondaryButtonColor = const Color(0xff4c4c4d);
+Color secondaryBackgroundColor = const Color(0xff161617);
+Color primaryTextColor = const Color(0xffbfbfbf);
